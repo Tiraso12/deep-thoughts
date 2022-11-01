b23f34529c33f20178095880d1b0a685be53f2a3
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-11-01 17:31:08 -0400

    adding auth and me display

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -21,8 +22,18 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -30,38 +41,32 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="container">
             <Routes>
-
               <Route
-                path='/'
+                path="/"
                 element={<Home />}
               />
               <Route
-                path='/login'
+                path="/login"
                 element={<Login />}
               />
               <Route
-                path='/signup'
+                path="/signup"
                 element={<Signup />}
               />
-              <Route path="/profile">
-                <Route path=":username" element={<Profile />} />
-                <Route path="" element={<Profile />} />
-              </Route>
+              <Route
+                path="/profile"
+                element={<Profile />}
+              />
               <Route
                 path="/thought/:id"
                 element={<SingleThought />}
               />
               <Route
-                path='/thought'
-                element={<SingleThought />}
-              />
-              <Route
-                path='*'
+                path="*"
                 element={<NoMatch />}
               />
             </Routes>
